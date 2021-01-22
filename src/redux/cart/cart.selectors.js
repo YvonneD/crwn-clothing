4a5e4367a6de9c4b796b245a38c7998388152bcd
Yifang Di <diyifang@ymail.com>
2021-01-22 15:45:32 -0500

    addItem and removeItem to checkout item component

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -22,7 +22,7 @@ export const selectCartItemsCount = createSelector(
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
     (accumulatedQuantity, cartItem) =>
-      (accumulatedQuantity + cartItem.quantity) * cartItem.price,
+      accumulatedQuantity + cartItem.quantity * cartItem.price,
     0
   )
 );
